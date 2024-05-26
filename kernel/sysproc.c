bbac514af9744a2c41307c6fd3779e27472866a2
noamgilad <noamgilad98@gmail.com>
2024-05-26 18:06:13 +0300

    1

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -89,3 +89,11 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+//This system call should return the size of the running process’ memory in bytes:
+uint64
+sys_memsize(void)
+{
+  return myproc()->sz;
+}
+
